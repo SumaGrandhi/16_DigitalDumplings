7c8b45c657cf95559904ed5fa71002f6d2360bf7
SumaGrandhi <106604431+SumaGrandhi@users.noreply.github.com>
2023-02-26 10:49:36 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require("cors");
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const fileUpload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-testAPIRouter = require('./routes/testAPI');
-var app = express();
+const app = express();
+const port = process.env.PORT || 3000;
+//app.get("/categories",(req,res)=>{res.redirect("http://localhost:3000/explore-latest")})
+require('dotenv').config();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.urlencoded( { extended: true } ));
+app.use(express.static('public'));
+app.use(expressLayouts);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser('CookingBlogSecure'));
+app.use(session({
+  secret: 'CookingBlogSecretSession',
+  saveUninitialized: true,
+  resave: true
+}));
+app.use(flash());
+app.use(fileUpload());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/testAPI',testAPIRouter);
+app.set('layout', './layouts/main');
+app.set('view engine', 'ejs');
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+const routes = require('./server/routes/recipeRoutes.js')
+app.use('/', routes);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get("*",(req,res)=>res.send("<h1>404 Page not found</h1>"))
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(port, ()=> console.log(`Listening to port ${port}`));
